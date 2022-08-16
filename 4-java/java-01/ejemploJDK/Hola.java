88b2f64f970451a869ce3f9915c06f2bf73950de
Kim0i <belazki@hotmail.com>
2022-08-16 16:18:42 -0500

    Intro a Java

---
+++ 4-java/java-01/ejemploJDK/Hola.java
@@ -0,0 +1,13 @@
+public class Hola{
+//Esto es una clase del JDK Java
+
+//String miVariable = "Hola mundo java";
+
+ 	public static void main (String [] arg){
+	String miVariable = "Hola mundo java";
+	String miNombre = "Fabiola"
+
+	//esta es la declaración del método main
+		System.out.println (miVariable);
+	}
+}
