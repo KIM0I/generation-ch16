c859cfd4c320d061dff1ab9963a252e631e9378f
Kim0i <belazki@hotmail.com>
2022-08-29 17:36:55 -0500

    Clases abstractas

---
+++ 4-java/PooAvanzado/PooAvanzado/src/com/fgm/h_abstracta/FigurasGeometricas.java
@@ -0,0 +1,9 @@
+package com.fgm.h_abstracta;
+
+//Class abstract usa métodos abstractos, es decir que no hay una definición del método
+
+public abstract class FigurasGeometricas {
+    public abstract float area();
+
+
+}
