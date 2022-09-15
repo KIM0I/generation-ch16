c714a7c0e4d4f88a5d4ab9ade8fc05bc186579d7
Kim0i <belazki@hotmail.com>
2022-09-15 15:21:24 -0500

    API

--- 7-spring/Spring Jpa-Hibernate/SpringJpa/src/main/java/com/generation/fgm/controllers/UsuarioController.java
+++ 7-spring/Spring Jpa-Hibernate/SpringJpa/src/main/java/com/generation/fgm/controllers/UsuarioController.java
@@ -1,12 +1,19 @@
 package com.generation.fgm.controllers;
 
 import java.util.ArrayList;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.fgm.models.UsuarioModel;
@@ -14,6 +21,8 @@ import com.generation.fgm.services.UsuarioService;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE,
+        RequestMethod.PUT })
 public class UsuarioController {
 	@Autowired
 	UsuarioService usuarioService;
@@ -26,5 +35,23 @@ public class UsuarioController {
 	public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
         return this.usuarioService.guardarUsuario(usuario);
     }
+	@GetMapping(path = "/{id}")
+	public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id) {
+        return this.usuarioService.obtenerPorId(id);
+    }
+	@GetMapping("/query")
+	public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return this.usuarioService.obtenerPorPrioridad(prioridad);
+    }
+	@DeleteMapping(path = "/{id}")
+	public String eliminarPorId(@PathVariable("id") Long id){
+        boolean ok = this.usuarioService.eliminarUsuario(id);
+        if (ok){
+            return "Se eliminó el usuario con id " + id;
+        }else{
+            return "No pudo eliminar el usuario con id" + id;
+        }
+    }
+
 
 }
--- 8-apiRest/api-rest-16/src/main/java/com/generation/mvc/models/UsuarioModel.java
+++ 8-apiRest/api-rest-16/src/main/java/com/generation/mvc/models/UsuarioModel.java
@@ -1,18 +1,29 @@
-package com.generation.fgm.models;
+package com.generation.mvc.models;
+
+import java.util.List;
 
 import javax.persistence.*;
 
+
+
 @Entity
-@Table(name= "usuario")
+@Table(name = "usuario")
 public class UsuarioModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(unique = true, nullable = false)
-
+	@Column(nullable = false)
 	private Long id;
 	private String nombre;
 	private String correo;
 	private Integer prioridad;
+	
+	@OneToMany(mappedBy = "user")
+    private List<PostModel> posts;
+	
+	@OneToOne(mappedBy = "usuario_id")
+	private DireccionModel direccion;
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -37,6 +48,17 @@ public class UsuarioModel {
 	public void setPrioridad(Integer prioridad) {
 		this.prioridad = prioridad;
 	}
-	
+	public List<PostModel> getPosts() {
+		return posts;
+	}
+	public void setPosts(List<PostModel> posts) {
+		this.posts = posts;
+	}
+	public DireccionModel getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(DireccionModel direccion) {
+		this.direccion = direccion;
+	}	
 	
 }
